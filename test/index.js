5d47dbcf231a63245ab32bf4570dea11fb94477d
Titus Wormer <tituswormer@gmail.com>
2018-11-17 13:34:45 +0100

    Update dev-dependencies

--- test/index.js
+++ test/index.js
@@ -29,7 +29,7 @@ test('format', function(t) {
 
     try {
       config = JSON.parse(fs.readFileSync(path.join(base, 'config.json')))
-    } catch (err) {}
+    } catch (error) {}
 
     rehype()
       .data('settings', {fragment: true})
