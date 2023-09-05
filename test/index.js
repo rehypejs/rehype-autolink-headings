2a2783ac13d4af5f6ef5ddd47d57a890e56d03e8
Titus Wormer <tituswormer@gmail.com>
2023-09-05 12:46:12 +0200

    Refactor to use `node:test`

--- test/index.js
+++ test/index.js
@@ -2,94 +2,70 @@
  * @typedef {import('../index.js').Options} Options
  */
 
-import fs from 'node:fs'
-import path from 'node:path'
-import {bail} from 'bail'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import test from 'node:test'
 import {rehype} from 'rehype'
-import {readSync} from 'to-vfile'
 import {isHidden} from 'is-hidden'
 import rehypeAutolinkHeadings from '../index.js'
 
-test('rehypeAutolinkHeadings', (t) => {
-  const root = path.join('test', 'fixtures')
-
-  t.test('fixtures', (t) => {
-    fs.readdir(root, (error, files) => {
-      bail(error)
-
-      const visible = files.filter((d) => !isHidden(d))
-
-      t.plan(visible.length)
-
-      let index = -1
-
-      while (++index < visible.length) {
-        one(visible[index])
-      }
-    })
-
-    /**
-     * @param {string} fixture
-     */
-    function one(fixture) {
-      const base = path.join(root, fixture)
-      const input = readSync(path.join(base, 'input.html'))
-      const output = readSync(path.join(base, 'output.html'))
-      /** @type {Options|undefined} */
-      let config
-
-      try {
-        config = JSON.parse(
-          String(fs.readFileSync(path.join(base, 'config.json')))
-        )
-      } catch {}
-
-      t.test(fixture, (t) => {
-        rehype()
-          .data('settings', {fragment: true})
-          .use(rehypeAutolinkHeadings, config)
-          .process(input, (error) => {
-            t.plan(3)
-            t.ifErr(error, 'shouldn’t throw')
-            t.equal(input.messages.length, 0, 'shouldn’t warn')
-            t.equal(String(input), String(output), 'should match')
-          })
-      })
-    }
-  })
-
-  t.test('functions', (t) => {
-    t.plan(3)
-
-    rehype()
+test('rehypeAutolinkHeadings', async function (t) {
+  await t.test('should support functions', async function () {
+    const file = await rehype()
       .data('settings', {fragment: true})
       .use(rehypeAutolinkHeadings, {
         behavior: 'after',
         group(node) {
-          t.equal(
-            node.properties && node.properties.id,
-            'a',
-            'should pass `node` to `group`'
-          )
+          assert.equal(node.properties.id, 'a')
           return {type: 'element', tagName: 'div', properties: {}, children: []}
         },
         content(node) {
-          t.equal(
-            node.properties && node.properties.id,
-            'a',
-            'should pass `node` to `content`'
-          )
+          assert.equal(node.properties.id, 'a')
           return {type: 'element', tagName: 'i', properties: {}, children: []}
         }
       })
-      .process('<h1 id=a>b</h1>', (error, file) => {
-        t.deepEqual(
-          [error, (file || {messages: []}).messages.length, String(file)],
-          [undefined, 0, '<div><h1 id="a">b</h1><a href="#a"><i></i></a></div>']
-        )
-      })
-  })
+      .process('<h1 id=a>b</h1>')
 
-  t.end()
+    assert.deepEqual(
+      String(file),
+      '<div><h1 id="a">b</h1><a href="#a"><i></i></a></div>'
+    )
+  })
+})
+
+test('fixtures', async function (t) {
+  const root = new URL('fixtures/', import.meta.url)
+  const folders = await fs.readdir(root)
+  let index = -1
+
+  while (++index < folders.length) {
+    const folder = folders[index]
+
+    if (isHidden(folder)) {
+      continue
+    }
+
+    await t.test(folder, async function () {
+      const folderUrl = new URL(folder + '/', root)
+      const input = await fs.readFile(new URL('input.html', folderUrl))
+      const output = String(
+        await fs.readFile(new URL('output.html', folderUrl))
+      )
+      /** @type {Options | undefined} */
+      let config
+
+      try {
+        config = JSON.parse(
+          String(await fs.readFile(new URL('config.json', folderUrl)))
+        )
+      } catch {}
+
+      const file = await rehype()
+        .data('settings', {fragment: true})
+        .use(rehypeAutolinkHeadings, config)
+        .process(input)
+
+      assert.equal(String(file), output)
+    })
+  }
 })
