bd010638a9ada8be91b9b3dda66b56b748a1d0b9
Titus Wormer <tituswormer@gmail.com>
2024-09-16 12:59:21 +0200

    Fix typo

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-/**
- * @typedef {import('./lib/index.js').Behavior} Behavior
- * @typedef {import('./lib/index.js').Build} Build
- * @typedef {import('./lib/index.js').BuildProperties} BuildProperties
- * @typedef {import('./lib/index.js').Options} Options
- */
-
+// Note: types exposed from `index.d.ts`.
 export {default} from './lib/index.js'
